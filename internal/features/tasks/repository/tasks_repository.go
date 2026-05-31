f48b5ccc90f378a98cd3d6c24797a982fc91c19d
Nikita-2727 <provotorovnikita@gmail.com>
2026-05-31 19:33:33 +0300

    temp

--- internal/features/tasks/repository/tasks_repository.go
+++ internal/features/tasks/repository/tasks_repository.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log/slog"
 	"strings"
 	"time"
 
@@ -236,7 +235,7 @@ func (r *TasksRepo) ClaimNextTask(ctx context.Context, statusPending, statusProc
 		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, error_type.NewNotFound("no pending tasks")
 		}
-		slog.Error("ClaimNextTask SQL error", "err", err)
+		fmt.Println("ClaimNextTask SQL error !!!!!!!!!!!!!!!!!!!", "err", err)
 		return nil, error_type.NewInternal(fmt.Errorf("select next tasks in queue: %w", err))
 	}
 
@@ -257,7 +256,7 @@ func (r *TasksRepo) ClaimNextTask(ctx context.Context, statusPending, statusProc
 	// обновляем статус задачи, чтобы другая горутина уже не могла ее взять
 	_, err = tx.Exec(ctx, `UPDATE tasks SET status = $1, started_at = NOW(), stage_entered_at = NOW() WHERE id = $2;`, statusProcessing, task.TaskID)
 	if err != nil {
-		slog.Error("ClaimNextTask SQL error", "err", err)
+		fmt.Println("ClaimNextTask SQL error !!!!!!!!!", "err", err)
 		return nil, error_type.NewInternal(fmt.Errorf("update next tasks in queue: %w", err))
 	}
 
